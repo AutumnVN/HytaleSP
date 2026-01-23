70e36b26071b4cdccaf849131126e36eea666d2b
Li <li@silica.codes>
2026-01-24 12:57:09 +1300

    Update linux

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-flatpak install org.freedesktop.Sdk//25.08 org.flatpak.Builder
+flatpak install org.freedesktop.Sdk//25.08 org.flatpak.Builder --system -y
 
 cd flatpak || exit
 ./buildpak.sh
